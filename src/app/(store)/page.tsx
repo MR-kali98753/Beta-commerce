81a1992ff91b25642f4f347b37b87089f409f885
MR kali <mrkali98753@gmail.com>
2025-04-08 12:40:45 +0530

    first commit

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,18 @@
+import BlackFridayBanner from "@/components/BlackFridayBanner";
+import ProductsView from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProduct } from "@/sanity/lib/products/getAllProducts";
+
+
+export default async function Home() {
+  const products = await getAllProduct() || []
+  const categories = await getAllCategories() || []
+  return (
+    <div>
+      <BlackFridayBanner />
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={categories} />
+      </div>
+    </div>
+  );
+}
---
+++ src/sanity/lib/live.ts
@@ -0,0 +1,21 @@
+// import "server only"
+import { defineLive } from "next-sanity";
+import { client } from './client'
+
+
+const token = process.env.SANITY_API_READ_TOKEN
+if (!token) {
+  throw new Error("Missing Sanity API read token")
+}
+
+export const { sanityFetch, SanityLive } = defineLive({ 
+  client: client.withConfig({ 
+    apiVersion: 'vX' 
+  }),
+  serverToken: token,
+  browserToken: token,
+  fetchOptions: {
+    revalidate: 0
+  } 
+});
+
